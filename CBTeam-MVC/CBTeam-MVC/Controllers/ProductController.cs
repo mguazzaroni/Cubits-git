64e7733ca1fd8f730a94e81cee5ed94921a73423
Matias Guazzaroni <matiasguazzaroni10@gmail.com>
2023-09-24 20:49:56 -0300

    Agrego controlador Product

---
+++ CBTeam-MVC/CBTeam-MVC/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using CBTeam_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CBTeam_MVC.Controllers
+{
+    [Route("Products")]
+    public class ProductController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            Product product = new Product
+            {
+                Name = "Fernet",
+                Description = "Fernet Branca",
+                Stock = 30,
+                Price = 2300,
+            };
+
+            return View(product);
+        }
+    }
+}
---
+++ CBTeam-MVC/CBTeam-MVC/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace CBTeam_MVC.Models
+{
+    public class Product
+    {
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public int? Stock { get; set; }
+
+        public decimal? Price { get; set; }
+    }
+}
